5806c5d42340bc08972701180aeed1aa108fe04c
Thomas Pope <tpope0928@gmail.com>
2019-01-09 22:43:44 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,5 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :students, :grade, :integer
-    add_column :students, :birthdate, :string
+    change_column :students, :birthdate, :datetime
   end
 end
